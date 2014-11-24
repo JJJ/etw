3fd172ea7efeb1c37dde397520b4bf120fe539a9
John James Jacoby <johnjamesjacoby@me.com>
2014-11-24 17:21:52 -0600

    Update batcache.

--- content/mu-plugins/batcache.php
+++ content/mu-plugins/batcache.php
@@ -12,39 +12,54 @@
 global $batcache;
 
 // Do not load if our advanced-cache.php isn't loaded
-if ( ! is_object( $batcache ) || ! method_exists( $wp_object_cache, 'incr' ) ) {
+if ( empty( $batcache ) || ! is_object( $batcache ) || ! method_exists( $wp_object_cache, 'incr' ) ) {
 	return;
 }
 
 $batcache->configure_groups();
 
-// Regen home and permalink on posts and pages
-add_action('clean_post_cache', 'batcache_post');
+/**
+ * Regen home and permalink on posts and pages
+ *
+ * @param  int $post_id
+ * @return If post revision or post is not published
+ */
+function batcache_post( $post_id = 0 ) {
 
-// Regen permalink on comments (TODO)
-//add_action('comment_post',          'batcache_comment');
-//add_action('wp_set_comment_status', 'batcache_comment');
-//add_action('edit_comment',          'batcache_comment');
-
-function batcache_post($post_id) {
-	global $batcache;
-
-	$post = get_post($post_id);
-	if ( $post->post_type === 'revision' || get_post_status( $post_id ) !== 'publish' ) {
+	// Bail on revisions or non-published posts
+	if ( wp_is_post_revision( $post_id ) || ( 'publish' !== get_post_status( $post_id ) ) ) {
 		return;
 	}
 
-	batcache_clear_url( get_option( 'home' ) );
-	batcache_clear_url( trailingslashit( get_option( 'home' ) ) );
+	// Get home option
+	$home = get_option( 'home' );
+
+	// Clear caches
+	batcache_clear_url( $home );
+	batcache_clear_url( trailingslashit( $home ) );
 	batcache_clear_url( get_permalink( $post_id ) );
 }
+add_action( 'clean_post_cache', 'batcache_post' );
 
+/**
+ * Used by batcache_post() to clean caches
+ *
+ * @global string $batcache
+ * @param  string $url
+ * @return boolean
+ */
 function batcache_clear_url( $url = '' ) {
 	global $batcache;
+
+	// Bail if no URL
 	if ( empty( $url ) ) {
 		return false;
 	}
-	$url_key = md5( $url );
-	wp_cache_add( "{$url_key}_version", 0, $batcache->group );
-	return wp_cache_incr( "{$url_key}_version", 1, $batcache->group );
+
+	$url_key   = md5( $url );
+	$cache_key = $url_key . '_version';
+
+	wp_cache_add( $cache_key, 0, $batcache->group );
+
+	return wp_cache_incr( $cache_key, 1, $batcache->group );
 }
