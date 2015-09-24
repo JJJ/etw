053ce03d9882583ee2d974127f9886a151f18d5c
John James Jacoby <johnjamesjacoby@me.com>
2015-09-24 17:34:49 -0500

    Fix syntax error.

--- content/plugins/wp-comment-humility/wp-comment-humility.php
+++ content/plugins/wp-comment-humility/wp-comment-humility.php
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
 // Actions
 add_action( 'admin_menu',             '_wp_comment_humility' );
 add_action( 'admin_head-comment.php', '_wp_comment_humility_modify_admin_menu_highlight' );
-add_action( 'plugins_loaded', '_wp_comment_humility_i18n' ) );
+add_action( 'plugins_loaded',         '_wp_comment_humility_i18n' );
 
 /**
  * Unset top level "Comments" menu and related submenus, then recreate it under
@@ -77,7 +77,5 @@ function _wp_comment_humility_modify_admin_menu_highlight() {
  * @since 0.1.0
  */
 function _wp_comment_humility_i18n() {
-
-	/* Load the translation of the plugin. */
 	load_plugin_textdomain( 'wp-comment-humility', false, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
