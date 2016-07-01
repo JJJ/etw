c569114cb5e9b262114e6c21fb2994b28dc8ca02
John James Jacoby <johnjamesjacoby@me.com>
2016-07-01 10:09:37 -0500

    MU plugin header

--- content/mu-plugins/make-mods.php
+++ content/mu-plugins/make-mods.php
@@ -1,4 +1,12 @@
 <?php
 
+/**
+ * Plugin name: Flox - Make Overrides (Network)
+ * Description: Filter Make settings
+ * Author:      The Flox Team
+ * Author URI:  https://flox.io
+ * Version:     0.0.1
+ */
+
 // No credit
-add_filter( 'make_show_footer_credit', '__return_false' );
+add_filter( 'make_show_footer_credit', '__return_false', PHP_INT_MAX );
