017d9abd65851d77eefb2f17dc3374d9667c32e3
John James Jacoby <johnjamesjacoby@me.com>
2016-07-01 10:05:07 -0500

    Update WP Multi Network.

--- content/plugins/wp-multi-network/includes/functions.php
+++ content/plugins/wp-multi-network/includes/functions.php
@@ -58,6 +58,15 @@ function user_has_networks( $user_id = 0 ) {
 		$user_login = $user_info->user_login;
 	}
 
+	// This filter can be used to short-circuit the process of retrieving network IDs of a user
+	$my_networks = apply_filters( 'networks_pre_user_is_network_admin', null, $user_id );
+	if ( null !== $my_networks ) {
+		if ( empty( $my_networks ) ) {
+			$my_networks = false;
+		}
+		return apply_filters( 'networks_user_is_network_admin', $my_networks, $user_id );
+	}
+
 	// Setup the networks array
 	$my_networks = array();
 
@@ -65,16 +74,9 @@ function user_has_networks( $user_id = 0 ) {
 	if ( is_multisite() ) {
 
 		// Get the network admins
-		$sql        = "SELECT site_id, meta_value FROM {$wpdb->sitemeta} WHERE meta_key = %s";
-		$query      = $wpdb->prepare( $sql, 'site_admins' );
-		$all_admins = $wpdb->get_results( $query );
-
-		foreach( (array) $all_admins as $network ) {
-			$network_admins = maybe_unserialize( $network->meta_value );
-			if ( in_array( $user_login, $network_admins, true ) ) {
-				$my_networks[] = (int) $network->site_id;
-			}
-		}
+		$sql         = "SELECT site_id FROM {$wpdb->sitemeta} WHERE meta_key = %s AND meta_value LIKE %s";
+		$query       = $wpdb->prepare( $sql, 'site_admins', '%"' . $user_login . '"%' );
+		$my_networks = array_map( 'intval', $wpdb->get_col( $query ) );
 	}
 
 	// If there are no networks, return false
