e768219eaae27a1c51beaeab289b1f21d24cef56
Chris Smith <chris@cgsmith.net>
2016-08-05 16:48:08 -0500

    Language for wp-tracking-code

--- content/plugins/wp-tracking-code/wp-tracking-code.php
+++ content/plugins/wp-tracking-code/wp-tracking-code.php
@@ -58,14 +58,14 @@ function wp_tracking_code_form(){
             <h2><?php esc_html_e('Tracking Code', 'wp-tracking-code');?></h2>
             <p><?php esc_html_e('Add web tracking code to html head or footer section.', 'wp-tracking-code');?></p>
             <?php if( !empty( $isSaved ) ) : ?>
-                <div class="updated"><p><strong><?php echo "Saved Successfully."; ?></strong></p></div>
+                <div class="updated"><p><strong><?php esc_html_e('Saved Successfully.', 'wp-tracking-code'); ?></strong></p></div>
             <?php endif; ?>
             <p>
-            <h3>Add Tracking Code to HTML head</h3>
+            <h3><?php esc_html_e('Add Tracking Code to HTML head', 'wp-tracking-code');?></h3>
             <textarea rows="20" style="width:100%" name="data[tracking_head][code]"><?php echo @$data['tracking_head']['code'] ?></textarea>
             <br />
             <input type="checkbox" name="data[tracking_head][disable]" id="tracking_head_disable" <?php checked( @$data['tracking_head']['disable'], 'on' ); ?>  />
-            <label for="tracking_head_disable">Disable this head tracking code</label>
+            <label for="tracking_head_disable"><?php esc_html_e('Disable this head tracking code', 'wp-tracking-code');?></label>
             </p>
 
             <?php wp_nonce_field( 'wp_tracking_code' ); ?>
