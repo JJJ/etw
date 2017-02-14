0e673a62352c27f78f3aead6f4cf7ffe192d050d
John James Jacoby <johnjamesjacoby@me.com>
2017-02-14 13:18:46 -0600

    Add barletta theme for BldgOrange.

---
+++ content/themes/barletta/js/barletta.scripts.js
@@ -0,0 +1,45 @@
+jQuery(function($) {
+  "use strict";
+
+// hide #back-top first
+$("#back-top").hide();
+
+// fade in #back-top
+
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 100) {
+		$('#back-top').fadeIn();
+	} else {
+		$('#back-top').fadeOut();
+	}
+});
+
+// scroll body to 0px on click
+$('#back-top a').on("click", function(){
+	$('body,html').animate({
+		scrollTop: 0
+	}, 800);
+	return false;
+});
+
+// add third menu for navwalker
+$(document).ready(function(){
+	$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+		event.preventDefault(); 
+		event.stopPropagation(); 
+		$(this).parent().siblings().removeClass('open');
+		$(this).parent().toggleClass('open');
+	});
+});
+
+// slider
+$('.mz-slider').bxSlider({
+	pager: false,
+	speed: 1000,
+	slideMargin: 0,
+	prevText: '<i class="fa fa-angle-left"></i>',
+	nextText: '<i class="fa fa-angle-right"></i>',
+	easing: 'ease-in-out'
+});
+
+});
