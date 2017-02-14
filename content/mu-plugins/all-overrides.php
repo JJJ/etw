d19ad4709bdd6ddaa23c02bc165898d309953ac2
johnjamesjacoby <johnjamesjacoby@me.com>
2017-02-14 14:54:46 -0600

    Barletta overrides.

--- content/mu-plugins/all-overrides.php
+++ content/mu-plugins/all-overrides.php
@@ -13,3 +13,23 @@ defined( 'ABSPATH' ) || exit();
 
 // Activity Humility
 add_filter( 'wp_user_activity_menu_humility', '__return_true' );
+
+// Themes
+add_action( 'init', function() {
+        remove_action( 'barletta_footer', 'barletta_footer_credits' );
+} );
+
+/**
+ * Footer credits
+ */
+function etw_footer_credits() {
+        ?>
+        <div class="site-info">
+        <?php if (get_theme_mod('barletta_footer_text') == '') { ?>
+        &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?><?php esc_html_e('. All rights reserved.', 'barletta'); ?>
+        <?php } else { echo esc_html( get_theme_mod( 'barletta_footer_text', 'barletta' ) ); } ?>
+        </div><!-- .site-info -->
+
+        <?php
+}
+add_action( 'barletta_footer', 'etw_footer_credits' );
